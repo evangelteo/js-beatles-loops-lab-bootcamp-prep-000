fddd3f2a3a3e13cba3502b71fa429b9786e24cd7
Evangel Teo <evangelteo@gmail.com>
2018-05-21 10:28:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(array1,array2) {
+  var newarray = []
+  for (let i = 0; i < array1.length;i++) {
+    newarray.push(array1[i] + " plays " + array2[i])
+  }
+  return newarray
+}
+
+function johnLennonFacts(array) {
+  var newarray = []
+  while (array != []) {
+    newarray.push(array.pop()+"!!!")
+  }
+  return newarray
+}
